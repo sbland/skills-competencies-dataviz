46015eae5fd95ff2c10ca9b7464349a62814a3dd
Sam Bland <sam.bland@york.ac.uk>
2025-03-14 13:24:14 +0000

    Add d3js barplot example

--- lib/data-viz/bar-plot.tsx
+++ lib/data-viz/bar-plot.tsx
@@ -1,46 +1,163 @@
 import * as d3 from "d3";
 
+export interface IDataItem {
+  skill: string;
+  category: string;
+  lvl: number;
+}
+const categoryAxisLabelsHeight = 30;
+
 export interface IBarPlotProps {
-  data: number[];
+  data: IDataItem[];
   width?: number;
   height?: number;
   marginTop?: number;
   marginRight?: number;
   marginBottom?: number;
   marginLeft?: number;
+  categoryPadding?: number;
+  skillPadding?: number;
+}
 
+function getColor(categories: string[]): (category: string) => string {
+  return d3
+    .scaleOrdinal()
+    .domain(categories)
+    .range(d3.schemeSpectral[categories.length])
+    .unknown("#ccc") as (category: string) => string;
 }
 
 export function BarPlot({
   data,
   width = 640,
   height = 400,
-  marginTop = 20,
   marginRight = 20,
   marginBottom = 20,
   marginLeft = 20,
+  categoryPadding = 40,
+  skillPadding = 20,
 }: IBarPlotProps) {
-  const domainX = [0, data.length - 1];
-  const rangeX = [marginLeft, width - marginRight];
+  const groupedByCategory = d3.group(data, (d) => d.category);
 
-  const x = d3.scaleLinear(
-    domainX,
-    rangeX,
+  const categoryIds = [...d3.union(data.map((d) => d.category)).keys()];
+
+  const sortedCategories = categoryIds.sort();
+
+  const color = getColor(sortedCategories);
+
+  /* Calculate the column widths based on the available width
+shared between the categories and the skills with padding
+included
+*/
+  const skillPaddingCount = sortedCategories.reduce((acc, category) => {
+    const count = (groupedByCategory.get(category)?.length ?? 0) - 1;
+    return acc + count;
+  }, 0);
+
+  const usableWidth = width - marginLeft - marginRight;
+
+  const columnWidth =
+    (usableWidth -
+      categoryPadding * (categoryIds.length -1) -
+      skillPadding * skillPaddingCount) /
+    data.length;
+
+  const categoryLeftPositions = sortedCategories.reduce(
+    (acc, category) => [
+      ...acc,
+      acc[acc.length - 1] +
+        (groupedByCategory.get(category)?.length ?? 0) * columnWidth +
+        ((groupedByCategory.get(category)?.length ?? 0) - 1) * skillPadding +
+        categoryPadding,
+    ],
+    [0],
   );
-  const y = d3.scaleLinear(d3.extent(data) as [number, number], [height - marginBottom, marginTop]);
-  const line = d3.line((_, i) => x(i), y);
-  return (
-    <svg width={width} height={height}>
-      <path
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        d={line(data) ?? undefined}
+  type Category = (typeof sortedCategories)[number];
+
+  const categoryWidthsMap: Record<Category, number> = sortedCategories.reduce(
+    (acc, category) => ({
+      ...acc,
+      [category]:
+        (groupedByCategory.get(category)?.length ?? 0) * columnWidth +
+        ((groupedByCategory.get(category)?.length ?? -99) - 1) * skillPadding,
+    }),
+    {},
+  );
+
+  const categoryLeftPositionsMap: Record<Category, number> =
+    categoryLeftPositions
+      .slice(0, -1)
+      .reduce((acc, v, i) => ({ ...acc, [sortedCategories[i]]: v }), {});
+
+  const catX = (categoryId: Category) =>
+    categoryLeftPositionsMap[categoryId] || -1;
+
+  const subX = (categoryId: Category, skillId: string) =>
+    (groupedByCategory.get(categoryId)?.findIndex((s) => s.skill == skillId) ??
+      0) *
+      columnWidth +
+    (groupedByCategory.get(categoryId)?.findIndex((s) => s.skill == skillId) ??
+      0) *
+      skillPadding;
+
+  const createAxisNode = (cat: string, dItems: IDataItem[]) => (
+    <g
+      key={cat}
+      fill={color(cat)}
+      className="categoryGroup"
+      data-category={cat}
+      transform={`translate(${catX(cat).toString()},${(height - marginBottom).toString()})`}
+    >
+      <rect
+        transform={`translate(0, -${categoryAxisLabelsHeight.toString()})`}
+        width={categoryWidthsMap[cat]}
+        height={categoryAxisLabelsHeight}
       />
-      <g fill="white" stroke="currentColor" strokeWidth="1.5">
-        {data.map((d, i) => (
-          <circle key={`${d.toString()}-${i.toString()}`} cx={x(i)} cy={y(d)} r="2.5" />
-        ))}
+      <text
+        transform={`translate(${(categoryWidthsMap[cat] / 2).toString()}, ${(
+          -categoryAxisLabelsHeight / 4
+        ).toString()})`}
+        fill="black"
+        textAnchor="middle"
+      >
+        {cat}
+      </text>
+      {dItems.map((d) => (
+        <g
+          key={`${cat}-${d.skill}`}
+          transform={`translate(${subX(d.category, d.skill).toString()}, -${(categoryAxisLabelsHeight + 10).toString()})`}
+          data-skill={d.skill}
+        >
+          <text
+            transform={`translate(${(columnWidth / 2).toString()}, 0)`}
+            fill="black"
+            textAnchor="middle"
+          >
+            {d.skill}
+          </text>
+          <text
+            transform={`translate(${(columnWidth / 2).toString()}, -20)`}
+            fill="black"
+            textAnchor="middle"
+          >
+            {d.lvl}
+          </text>
+          <rect
+            width={columnWidth}
+            height={d.lvl * 20}
+            transform={`scale(1), translate(0, -${(20 + d.lvl * 20).toString()})`}
+          />
+        </g>
+      ))}
+    </g>
+  );
+
+  return (
+    <svg width={width} height={height} style={{ border: "1px solid black" }}>
+      <g id="x-axis" transform={`translate(${marginLeft.toString()},0)`}>
+        {sortedCategories.map((c) =>
+          createAxisNode(c, groupedByCategory.get(c) ?? []),
+        )}
       </g>
     </svg>
   );
---
+++ src/stories/dataviz/bar-plot.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { BarPlot, IDataItem } from "../../../lib/data-viz/bar-plot";
+
+
+const demodata: IDataItem[] = [
+  { skill: "node", category: "Tech Skill", lvl: 7 },
+  { skill: "react", category: "Tech Skill", lvl: 6 },
+  { skill: "Gitf", category: "Tech Skill", lvl: 5 },
+  { skill: "Code", category: "Tech Skill", lvl: 1 },
+  { skill: "Mangroupr", category: "Leadership", lvl: 2 },
+  { skill: "PM", category: "Leadership", lvl: 1 },
+  { skill: "Leader", category: "Leadership", lvl: 5 },
+  { skill: "Networking", category: "Communication", lvl: 4 },
+  { skill: "Presenting", category: "Communication", lvl: 1 },
+];
+
+
+const meta = {
+  title: "DataViz/BarPlot",
+  component: BarPlot,
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof BarPlot>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    data: demodata,
+  },
+};
