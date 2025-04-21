b74e528ea4e5f7deb56b72eb20d00409bd783db4
Sam Bland <sam.bland@york.ac.uk>
2025-04-21 20:19:27 +0100

    add react to external in rollup config

--- vite.config.ts
+++ vite.config.ts
@@ -21,15 +21,15 @@ export default defineConfig({
       // make sure to externalize deps that shouldn't be bundled
       // into your library.
       // Commented out until React imported as UMD
-      // external: ["react", 'react-dom'],
-      // output: {
-      //   // Provide global variables to use in the UMD build
-      //   // for externalized deps
-      //   globals: {
-      //     react: "React",
-      //     'react-dom': "ReactDOM",
-      //   },
-      // },
+      external: ["react", 'react-dom'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: "React",
+          'react-dom': "ReactDOM",
+        },
+      },
     },
   },
 });
