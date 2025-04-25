c60c5d150e06185dd4b751c4d87eac833f157d6e
Sam Bland <sam.bland@york.ac.uk>
2025-04-25 09:06:29 +0100

    add options to render function

--- lib/data-viz/radial-plot/render.tsx
+++ lib/data-viz/radial-plot/render.tsx
@@ -2,11 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RadialBarChart } from "./radial-plot.tsx";
 import { IDataItem } from "../lib.ts";
+import { IRadialBarPlotProps } from "./lib.ts";
 
-export const renderRadialPlot = (rootId: string, data: IDataItem[]) => {
+export const renderRadialPlot = (
+  rootId: string,
+  data: IDataItem[],
+  options: Omit<IRadialBarPlotProps, "data">,
+) => {
   createRoot(document.getElementById(rootId)!).render(
     <StrictMode>
-      <RadialBarChart data={data} />
+      <RadialBarChart data={data} {...options} />
     </StrictMode>,
   );
 };
